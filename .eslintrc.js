d638c0f25c2c236afc29fd31fdaca6d4c8461728
junjie <junjie@kooboo.net>
2021-11-25 16:31:20 +0800

    chore: add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: [1, 'single'],
-    'space-before-function-paren': 0,
-    'comma-dangle': ['error', 'only-multiline'],
+    'quotes': [1, 'single'], // 引号
+    'space-before-function-paren': 0, // 函数前空格
+    'comma-dangle': ['error', 'only-multiline'], // 拖尾逗号
+    'max-len': ['error', { code: 200 }], // 强制最大行长度
   },
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div class="home"></div>
   <el-button @click="printJobs">hello</el-button>
 </template>
 
 <script lang="ts" setup>
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 const printJobs = () => {
   console.log(1)
 }
-
 </script>
