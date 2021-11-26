e9502a2a915d775d4103a44e43d31256f264fd29
junjie <junjie@kooboo.net>
2021-11-26 11:11:42 +0800

    chore: add webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,35 @@
 const path = require('path')
 const { resolve } = path
 module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/styles/utils/variable.scss";`, //在 sass-loader v8 中，这个选项名是 "prependData"，其他为 "additionalData"
+      },
+    },
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('import-glob')
+      .test(/\.scss/)
+      .use('import-glob-loader2')
+      .loader('import-glob-loader2')
+      .end()
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        // 修改它的选项...
+        return options
+      })
+  },
   devServer: {
     // hot: true,
     // liveReload: true,
@@ -13,13 +42,6 @@ module.exports = {
         pathRewrite: {
           '^/api': '/',
         },
-      },
-    },
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/assets/styles/utils/variable.scss";`, //在 sass-loader v8 中，这个选项名是 "prependData"，其他为 "additionalData"
       },
     },
   },
